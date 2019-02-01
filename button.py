93159efde3f8558a3085fd1819744dfe279e3848
adyg <yogamuris@gmail.com>
2019-02-01 21:06:38 +0700

    fixing typo code

--- button.py
+++ button.py
@@ -16,7 +16,7 @@ class Button():
         self.prep_msg(msg)
 
     def prep_msg(self, msg):
-        self.msg_image = self.font.render(msg, True, self.tect_color, self.button_color)
+        self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
         self.msg_image_rect = self.msg_image.get_rect()
         self.msg_image_rect.center = self.rect.center
 
