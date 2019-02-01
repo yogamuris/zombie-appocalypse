375f185fe7edae8c87cc999c3403ec66ae2262a8
adyg <yogamuris@gmail.com>
2019-02-01 21:03:21 +0700

    first init

---
+++ bullet.py
@@ -0,0 +1,24 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+
+    def __init__(self, ai_settings, screen, zombie):
+        super(Bullet, self).__init__()
+        self.screen = screen
+
+        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
+        self.rect.centerx = fighter.rect.centerx
+        self.rect.top = fighter.rect.top
+
+        self.y = float(self.rect.y)
+
+        self.color = ai_settings.bullet_color
+        self.speed_factor = ai_settings.bullet_speed_factor
+
+    def update(self):
+        self.y -= self.speed_factor
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
